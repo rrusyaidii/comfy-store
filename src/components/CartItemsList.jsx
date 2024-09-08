7986b8e7bf9ca9500b79b14898f78b84342fbff0
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2024-09-09 00:18:46 +0800

    commit

---
+++ src/components/CartItemsList.jsx
@@ -0,0 +1,4 @@
+const CartItemsList = () => {
+  return <h1>CartItemsList</h1>;
+};
+export default CartItemsList;
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,6 +2,8 @@ import { useLoaderData } from "react-router-dom";
 import { formatPrice, customFetch, generateAmountOptions } from "../utils";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "../features/cart/cartSlice";
 
 export const loader = async ({ params }) => {
   const response = await customFetch(`/products/${params.id}`);
@@ -19,6 +21,23 @@ const SingleProduct = () => {
 
   const handleAmount = (e) => {
     setAmount(parseInt(e.target.value));
+  };
+
+  const cartProduct = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image,
+    title,
+    price,
+    company,
+    productColor,
+    amount,
+  };
+
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(addItem({ product: cartProduct }));
   };
 
   return (
@@ -88,12 +107,7 @@ const SingleProduct = () => {
           </div>
           {/* CART BTN */}
           <div className="mt-10">
-            <button
-              className="btn btn-secondary btn-md"
-              onClick={() => {
-                console.log("add to bag");
-              }}
-            >
+            <button className="btn btn-secondary btn-md" onClick={addToCart}>
               Add to Bag
             </button>
           </div>
